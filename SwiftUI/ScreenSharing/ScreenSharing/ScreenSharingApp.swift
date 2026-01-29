93179fcf274136ecec88ba4abbd9e35f78085c83
aos-svg <artur.osinski@vonage.com>
2026-01-29 10:48:19 +0100

    VIDECO-11553 add capturer

---
+++ SwiftUI/ScreenSharing/ScreenSharing/ScreenSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenSharingApp.swift
+//  ScreenSharing
+//
+//  Created by Artur Osiński on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScreenSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
