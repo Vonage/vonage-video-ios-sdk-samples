024f9cd24690f3edd8d88268413c1150582c0eff
aos-svg <artur.osinski@vonage.com>
2026-01-29 10:48:52 +0100

    VIDECO-11553 move capturer

---
+++ SwiftUI/BasicVideoCapturer/BasicVideoCapturer/Wrap.swift
@@ -0,0 +1,21 @@
+//
+//  Wrap.swift
+//  BasicVideoChat
+//
+//  Created by Artur Osiński on 02/12/2025.
+//
+
+import UIKit
+import SwiftUI
+
+struct Wrap: UIViewRepresentable {
+    private let view: UIView
+    
+    init(_ view: UIView) {
+        self.view = view
+    }
+    
+    func makeUIView(context: Context) -> UIView { view }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
