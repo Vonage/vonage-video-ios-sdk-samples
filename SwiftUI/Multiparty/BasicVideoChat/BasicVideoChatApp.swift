e60f370ef8c1228b1ddd6606b5c0ad4f5618a5a4
aos-svg <artur.osinski@vonage.com>
2026-05-22 19:35:50 +0200

    VIDECO-11945 add multiparty

---
+++ SwiftUI/Multiparty/BasicVideoChat/BasicVideoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicVideoChatApp.swift
+//  BasicVideoChat
+//
+//  Created by Artur Osiński on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BasicVideoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
